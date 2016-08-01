3065a22e75a4563fc9ac9f103ce1df23d84a0d0f
aman_d1996 <aman_d1996@yahoo.com>
2016-08-01 13:29:05 +0530

    latest code

--- hack.sh
+++ hack.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-for i in {1..15}
+for i in {1..20}
 do
 	touch temp$i.txt
 	git add -A
+	a="Wed Feb "
+	b=$i
+	c=" 14:00 2016 +0530"
+	export GIT_AUTHOR_DATE=$a$b$c
+	export GIT_COMMITTER_DATE=$a$b$c
 	git commit -am "hack the code"
 	rm temp$i.txt
+	export GIT_AUTHOR_DATE=$a$b$c
+	export GIT_COMMITTER_DATE=$a$b$c
 	git commit -am "hack the code"
 done
